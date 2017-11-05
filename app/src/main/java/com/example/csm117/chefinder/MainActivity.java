b2b150dc59d628a4ff24a7616789b8add0bcb6d0
Stephanie Lam <sytl@yahoo.com>
2017-11-05 01:20:10 -0800

    added initial files + testing how Firebase db works

---
+++ app/src/main/java/com/example/csm117/chefinder/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.csm117.chefinder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        testMethod();
+    }
+
+    public void testMethod() {
+        FirebaseDatabase db = FirebaseDatabase.getInstance();
+        DatabaseReference ref = db.getReference("message"); // Key
+        ref.setValue("This is a test message"); // Value
+    }
+}
