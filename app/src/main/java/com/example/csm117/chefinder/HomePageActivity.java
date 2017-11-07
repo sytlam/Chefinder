2ae344eed7a84fa11270cbb2e49fc33c56fddc23
Stephanie Lam <sytl@yahoo.com>
2017-11-07 01:13:24 -0800

    add home page

---
+++ app/src/main/java/com/example/csm117/chefinder/HomePageActivity.java
@@ -0,0 +1,44 @@
+package com.example.csm117.chefinder;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class HomePageActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_groups:
+                    mTextMessage.setText(R.string.title_groups);
+                    return true;
+                case R.id.navigation_ingredients:
+                    mTextMessage.setText(R.string.title_ingredient_list);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
--- app/src/main/java/com/example/csm117/chefinder/MainActivity.java
+++ app/src/main/java/com/example/csm117/chefinder/MainActivity.java
@@ -1,18 +1,32 @@
 package com.example.csm117.chefinder;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button homepage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        testMethod();
+        //testMethod();
+        homepage = (Button)findViewById(R.id.homepage_button);
+
+        homepage.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                gotoHomepage();
+            }
+        });
     }
 
     public void testMethod() {
@@ -20,4 +34,11 @@ public class MainActivity extends AppCompatActivity {
         DatabaseReference ref = db.getReference("message"); // Key
         ref.setValue("This is a test message"); // Value
     }
+
+    private void gotoHomepage() {
+        Intent intent = new Intent(this, HomePageActivity.class);
+        startActivity(intent);
+    }
+
+
 }
